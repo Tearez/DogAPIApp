14f173a861a746232a22d364dad59cf9f3980d01
Martin Dimitrov <martinreff1@gmail.com>
2023-05-02 17:36:58 +0300

    Add empty app

---
+++ DogAPIApp/DogAPIApp/DogAPIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogAPIAppApp.swift
+//  DogAPIApp
+//
+//  Created by Martin Dimitrov on 2.05.23.
+//
+
+import SwiftUI
+
+@main
+struct DogAPIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
